e261c1299c9dccba14334d052975d63265d700bc
Navneetdubey06 <nd06works@gmail.com>
2025-03-11 15:30:45 +0530

    Initial commit: Upload project to GitHub

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../images/logo.png";
+
+const Footer = () => {
+    return (
+        <div className="w-full h-20 bg-amazon_light text-gray-400 flex items-center justify-center gap-4">
+            <p className="text-sm">
+                2024 by ND
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
